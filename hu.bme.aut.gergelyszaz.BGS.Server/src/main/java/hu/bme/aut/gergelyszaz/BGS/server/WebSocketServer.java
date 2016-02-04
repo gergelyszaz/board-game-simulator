9a33584c9e5e912e2e9416af790dc6a8cfe3ad76
MrGuill666 <gergelyszaz@gmail.com>
2016-02-04 23:36:40 +0100

    Added Maven to BGS and Server projects.
    Former-commit-id: dac86344b7378d1ed30d139e05693787a3420a1c

--- hu.bme.aut.gergelyszaz.BGS.Server/src/main/java/hu/bme/aut/gergelyszaz/BGS/server/WebSocketServer.java
+++ hu.bme.aut.gergelyszaz.BGS.Server/src/main/java/hu/bme/aut/gergelyszaz/BGS/server/WebSocketServer.java
@@ -1,28 +1,56 @@
 package hu.bme.aut.gergelyszaz.BGS.server;
- 
+
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
- 
+import java.util.Properties;
+
 import org.glassfish.tyrus.server.Server;
- 
+
+import hu.bme.aut.gergelyszaz.BGS.core.GameManager;
+
 public class WebSocketServer {
- 
-    public static void main(String[] args) {
-        runServer();
-    }
- 
-    public static void runServer() {
-        Server server = new Server("localhost", 8025, "/websockets", BGSServer.class);
- 
-        try {
-            server.start();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Please press a key to stop the server.");
-            reader.readLine();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            server.stop();
-        }
-    }
+
+	private static Properties configFile = new Properties();
+
+	public static void main(String[] args) {
+		runServer();
+	}
+
+	public static void runServer() {
+		
+		try {
+			InputStream input = new FileInputStream("games.properties");
+			configFile.load(input);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		for (Object k : configFile.values()) {
+			System.out.println((String) k);
+			try {
+				GameManager.getInstance().mm.LoadModel((String)k);
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		
+		Server server = new Server("localhost", 8025, "/websockets", BGSServer.class);
+		
+		try {
+			server.start();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Please press a key to stop the server.");
+			reader.readLine();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			server.stop();
+		}
+	}
+
+	public static void LoadAllModels() {
+		
+	}
 }
--- hu.bme.aut.gergelyszaz.BGS/src/hu/bme/aut/gergelyszaz/BGS/core/ModelManager.java
+++ hu.bme.aut.gergelyszaz.BGS/src/hu/bme/aut/gergelyszaz/BGS/core/ModelManager.java
@@ -1,12 +1,8 @@
 package hu.bme.aut.gergelyszaz.BGS.core;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Properties;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -20,7 +16,7 @@ import hu.bme.aut.gergelyszaz.bGL.Model;
 
 public class ModelManager {
 	private static XtextResourceSet resourceSet;
-	private static Properties configFile = new Properties();
+	
 	private Hashtable<String, Model> models=new Hashtable<String, Model>();
 
 	public void Initialize() {
@@ -28,26 +24,6 @@ public class ModelManager {
 		Injector injector = new BGLStandaloneSetup().createInjectorAndDoEMFRegistration();
 		resourceSet = injector.getInstance(XtextResourceSet.class);
 		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
-		
-		try {
-			InputStream input=new FileInputStream("games.properties");
-			configFile.load(input);
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-	
-	public void LoadAllModels()
-	{
-		for(Object k:configFile.values())
-		{
-			System.out.println((String)k);
-			try{
-				LoadModel((String)k);
-			} catch(Exception e){
-				System.err.println(e.getMessage());
-			}
-		}
 	}
 	
 	public List<String> AvailableModels()
