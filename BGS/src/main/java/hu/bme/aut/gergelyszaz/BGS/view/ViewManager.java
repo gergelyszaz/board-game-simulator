c6b76ef7a32d7eeea5a3f2ca4129174a5c441ca9
Gergely Száz <gergelyszaz@gmail.com>
2016-10-20 00:16:32 +0200

    ViewManager tested
    Former-commit-id: 04cd7c7b1853483bbbfa47c14aa2a032466d45bd

--- BGS/src/main/java/hu/bme/aut/gergelyszaz/BGS/view/ViewManager.java
+++ BGS/src/main/java/hu/bme/aut/gergelyszaz/BGS/view/ViewManager.java
@@ -1,8 +1,19 @@
 package hu.bme.aut.gergelyszaz.BGS.view;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by mad on 2016. 10. 19..
  */
 public class ViewManager {
+	Set<View> views=new HashSet<>();
+	public void add(View view){
+		views.add(view);
+	}
+
+	public void refreshViews(){
+		views.forEach(View::Refresh);
+	}
 
 }
