050190dac5150eb835590a6e9be90d39031bfc89
Gergely Száz <gergelyszaz@gmail.com>
2016-10-31 23:37:56 +0100

    SF
    Former-commit-id: a0f161a1cdd521ff13e1e6cad699d010bfeebfda

--- BGS/src/test/java/hu/bme/aut/gergelyszaz/BGS/manager/GameFactoryTest.java
+++ BGS/src/test/java/hu/bme/aut/gergelyszaz/BGS/manager/GameFactoryTest.java
@@ -1,11 +1,8 @@
 package hu.bme.aut.gergelyszaz.BGS.manager;
 
-import hu.bme.aut.gergelyszaz.BGS.game.Game;
-import hu.bme.aut.gergelyszaz.BGS.game.GameImpl;
-import hu.bme.aut.gergelyszaz.BGS.game.VariableManager;
+import hu.bme.aut.gergelyszaz.BGS.game.*;
 import hu.bme.aut.gergelyszaz.bGL.Model;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.Map;
 
@@ -15,62 +12,68 @@ import static org.junit.Assert.*;
  * Created by gergely.szaz on 2016. 10. 30..
  */
 public class GameFactoryTest {
-    GameFactory gameFactory;
-    ModelManager modelManager;
-    String rules="GAME TestGame\n" +
-            "PLAYERS 2{" +
-            "variable=10" +
-            " } \n" +
-            "BOARD {\n" +
-            "    x=1\n" +
-            "\tF1{tokenCount=2}\n" +
-            "}\n" +
-            "TOKEN token{}\n" +
-            "RULES {\n" +
 
-            "}";
+	GameFactory gameFactory;
+	ModelManager modelManager;
+	String rules = "GAME TestGame\n" + "PLAYERS 2{" + "variable=10" + " } \n" +
+			"BOARD {\n" + "    x=1\n" + "\tF1{tokenCount=2}\n" + "}\n" +
+			"TOKEN token{}\n" + "RULES {\n" + "}";
 
-    @Before
-    public void setUp() throws Exception {
-        gameFactory=new GameFactory();
-        modelManager=new ModelManager();
+	@Before
+	public void setUp() throws Exception {
 
-    }
+		gameFactory = new GameFactory();
+		modelManager = new ModelManager();
 
-    @Test
-    public void PlayerCountTest() throws Exception{
-        Model model=modelManager.LoadModel("GAME PlayerCountTest PLAYERS 1 {} BOARD { RULES {}}");
-        GameImpl game=gameFactory.CreateGame(model);
-        VariableManager variableManager=game.getVariableManager();
+	}
 
-        Map variables=variableManager.getVariables(null);
-        assertEquals(2,variables.size());
-        assertTrue(variableManager.getVariables(null).containsKey("currentPlayer"));
-    }
+	@Test
+	public void PlayerCountTest() throws Exception {
 
-    @Test
-    public void CommonPlayerVariableTest() throws Exception{
+		Model model = modelManager.LoadModel(
+				"GAME PlayerCountTest PLAYERS 1 {} BOARD { RULES {}}");
+		GameImpl game = gameFactory.CreateGame(model);
+		VariableManager variableManager = game.getVariableManager();
 
-        Model model=modelManager.LoadModel("GAME PlayerVariableTest PLAYERS 1 {a=5 b=9} BOARD { RULES {}}");
-        GameImpl game=gameFactory.CreateGame(model);
-        VariableManager variableManager=game.getVariableManager();
+		Map variables = variableManager.getVariables(null);
+		assertEquals(2, variables.size());
+		assertTrue(variableManager.getVariables(null)
+				.containsKey("currentPlayer"));
+	}
 
-        Map variables=variableManager.getVariables(null);
-        assertEquals(2,variables.size());
-        assertEquals(5,variableManager.getReference(variableManager.getReference(null,"currentPlayer"),"a"));
-        assertEquals(9,variableManager.getReference(variableManager.getReference(null,"currentPlayer"),"b"));
-    }
+	@Test
+	public void CommonPlayerVariableTest() throws Exception {
 
-    @Test
-    public void UniquePlayerVariableTest() throws Exception{
+		Model model = modelManager.LoadModel(
+				"GAME PlayerVariableTest PLAYERS 1 {a=5 b=9} BOARD { RULES {}}");
+		GameImpl game = gameFactory.CreateGame(model);
+		VariableManager variableManager = game.getVariableManager();
 
-        Model model=modelManager.LoadModel("GAME PlayerVariableTest PLAYERS 4 {a=5 b=9  } BOARD { RULES {}}");
-        GameImpl game=gameFactory.CreateGame(model);
-        VariableManager variableManager=game.getVariableManager();
+		Map variables = variableManager.getVariables(null);
+		assertEquals(2, variables.size());
+		assertEquals(5,
+				variableManager.getReference(variableManager.getReference(null,
+						"currentPlayer"), "a"));
+		assertEquals(9,
+				variableManager.getReference(variableManager.getReference(null,
+						"currentPlayer"), "b"));
+	}
 
-        Map variables=variableManager.getVariables(null);
-        assertEquals(2,variables.size());
-        assertEquals(5,variableManager.getReference(variableManager.getReference(null,"currentPlayer"),"a"));
-        assertEquals(9,variableManager.getReference(variableManager.getReference(null,"currentPlayer"),"b"));
-    }
+	@Test
+	public void UniquePlayerVariableTest() throws Exception {
+
+		Model model = modelManager.LoadModel(
+				"GAME PlayerVariableTest PLAYERS 4 {a=5 b=9 } BOARD { RULES {}}");
+		GameImpl game = gameFactory.CreateGame(model);
+		VariableManager variableManager = game.getVariableManager();
+
+		Map variables = variableManager.getVariables(null);
+		assertEquals(2, variables.size());
+		assertEquals(5,
+				variableManager.getReference(variableManager.getReference(null,
+						"currentPlayer"), "a"));
+		assertEquals(9,
+				variableManager.getReference(variableManager.getReference(null,
+						"currentPlayer"), "b"));
+	}
 }
