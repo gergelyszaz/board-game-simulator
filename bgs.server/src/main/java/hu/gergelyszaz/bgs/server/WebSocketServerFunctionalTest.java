240af5a6e9d42db6e3482f27eb7ad4809ff7bfff
Gergely Száz <gergelyszaz@gmail.com>
2018-06-29 23:02:06 +0200

    Clear up ServerFunctionalTest

--- bgs.server/src/main/java/hu/gergelyszaz/bgs/server/WebSocketServerFunctionalTest.java
+++ bgs.server/src/main/java/hu/gergelyszaz/bgs/server/WebSocketServerFunctionalTest.java
@@ -2,12 +2,11 @@ package hu.gergelyszaz.bgs.server;
 
 import static org.junit.Assert.fail;
 
+import java.io.Closeable;
 import java.net.URI;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.net.URISyntaxException;
 
 import org.eclipse.jetty.websocket.WebSocket;
-import org.eclipse.jetty.websocket.WebSocket.Connection;
 import org.eclipse.jetty.websocket.WebSocketClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,26 +23,47 @@ class WebSocketServerFunctionalTest {
 	void tearDown() {
 		WebSocketServer.stopServer();
 	}
+	
+	public static Client createClient(String name) throws Exception {
+		Client client = new Client("C1");
+		
+		URI uri = new URI("ws://localhost:8025/game");
+		
+		new WebSocketClient().open(uri, client);
+		
+		synchronized (client) {
+			client.wait();
+		}
+		
+		return client;
+	}
 
 	@Test
 	void testRunServer() throws Exception {
-		URI uri = new URI("ws://localhost:8025/game");
-		MessageListener messageListener = new MessageListener();
-		Future<Connection> connectionFuture = 
-			new WebSocketClient().open(uri, messageListener);
-		Connection connection = connectionFuture.get(2, TimeUnit.SECONDS);
-
-		connection.sendMessage("{\"action\":\"info\"}");
-
-		synchronized (messageListener) {
-			messageListener.wait();			
+		try (Client client1 = createClient("C1"); Client client2 = createClient("C2")) {
+			client1.sendMessage("info", "");
+			client1.sendMessage("join", "Mills");
+			client2.sendMessage("join", "Mills");
 		}
-		connection.close();
 	}
 
-	private static class MessageListener implements WebSocket.OnTextMessage {
+	private static class Client implements WebSocket.OnTextMessage, Closeable {
+		public Client(String name) {
+			this.name = name;
+		}
+		Connection connection;
+		private String name;
+
 		@Override
 		public void onOpen(Connection connection) {
+			this.connection = connection;
+			synchronized (this) {
+				this.notify();
+			}
+		}
+
+		public void close() {
+			connection.close();
 		}
 
 		@Override
@@ -53,17 +73,17 @@ class WebSocketServerFunctionalTest {
 
 		@Override
 		public void onMessage(String data) {
-			System.out.println(data);
-			
-			String expectedMessage = "{\"games\":[{\"name\":\"Mills\"},{\"name\":\"TicTacToe\"},{\"name\":\"Uno\"}],\"status\":\"ok\"}";
-			
-			if(data.contains(expectedMessage)) {
-				synchronized (this) {
-					this.notify();					
-				}
+			synchronized (connection) {
+				System.out.println(name+":\t"+data);
+				connection.notify();
+			}
+		}
 
+		public void sendMessage(String action, String parameter) throws Exception {
+			synchronized (connection) {
+				connection.sendMessage("{\"action\":\"" + action + "\",\"parameter\":\"" + parameter + "\"}");
+				connection.wait();
 			}
 		}
 	}
-
 }
